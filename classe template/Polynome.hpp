bda6de8f15fc169bfedbfe70033ae78ec349c208
U-ONL\juvigny <juvigny@onera.fr>
2019-02-18 14:37:48 +0100

    Seconde version non complete

--- classe template/Polynome.hpp
+++ classe template/Polynome.hpp
@@ -1,6 +1,7 @@
 #ifndef _POLYNOME_HPP_
 #define _POLYNOME_HPP_
 # include <vector>
+# include <cassert>
 
 template<typename K>
 class Polynome
@@ -19,11 +20,42 @@ public:
     ~Polynome() = default;
 
     int degree() const { return _coeff.size()-1; }
-    Polynome operator + ( const Polynome& p ) const;
+    Polynome operator + ( const Polynome& p ) const
+    {
+        Polynome r(std::max(p.degree(), this->degree()));
+        for ( int i = 0; i <= std::min(p.degree(), this->degree());
+             ++i)
+            r[i] = p[i] + this->_coeff[i];
+        if ( this->degree() > p.degree() )
+            for ( int i = p.degree() + 1; i <= this->degree();
+                   ++i )
+                r[i] = this->_coeff[i];
+        else if ( p.degree() > this->degree() )
+        {
+            for ( int i = this->degree()+1; i <= p.degree();
+                  ++i )
+                r[i] = p[i];
+        }
+        return r;
+    }
+
+    K& operator []( int i )
+    {
+        assert(i>=0);// Supprime si option -DNDEBUG mis à la compilation
+        assert(i<=degree());        
+        return _coeff[i];
+    }
+
+    K operator []( int i ) const
+    {
+        assert(i>=0);
+        assert(i<=degree());        
+        return _coeff[i];
+    }
 
     Polynome derivate() const;
     Polynome primitive( const K& p0 ) const;
-    
+
 private:
     container _coeff;
 };
--- classe template/test_polynome.cpp
+++ classe template/test_polynome.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Polynome.hpp"
 
 int main()
